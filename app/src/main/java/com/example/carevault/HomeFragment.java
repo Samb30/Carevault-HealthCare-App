932ee2ebf10283cdf5bf0737a0715059acd1c93e
UDAY B <udaybhati134@gmail.com>
2024-01-16 23:21:32 +0530

    booking part done 50%

--- app/src/main/java/com/example/carevault/HomeFragment.java
+++ app/src/main/java/com/example/carevault/HomeFragment.java
@@ -23,7 +23,7 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View view= inflater.inflate(R.layout.fragment_home, container, false);
         dctrall=view.findViewById(R.id.topdoctors);
         expanddoc=view.findViewById(R.id.Doctor);
