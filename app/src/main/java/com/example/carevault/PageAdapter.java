85c0a0fdbc62f780da8e909ec181973e071fb26c
saras1122 <sarasnagaria1@gmail.com>
2024-01-21 22:57:16 +0530

    Fragments added

---
+++ app/src/main/java/com/example/carevault/PageAdapter.java
@@ -0,0 +1,34 @@
+package com.example.carevault;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.carevault.tab.CancelledFragment;
+import com.example.carevault.tab.CompletedFragment;
+import com.example.carevault.tab.UpcomingFragment;
+
+public class PageAdapter  extends FragmentStateAdapter {
+    public PageAdapter(@NonNull Fragment fragment) {
+        super(fragment);
+    }
+
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new UpcomingFragment();
+            case 1:
+                return new CompletedFragment();
+            case 2:
+                return new CancelledFragment();
+            default:
+                return new UpcomingFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
---
+++ app/src/main/java/com/example/carevault/fragments/DoctorsFragment.java
@@ -0,0 +1,59 @@
+package com.example.carevault.fragments;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import com.example.carevault.PageAdapter;
+import com.example.carevault.R;
+import com.google.android.material.tabs.TabLayout;
+
+public class DoctorsFragment extends Fragment {
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    PageAdapter myViewPagerAdapter;
+    ImageButton pop;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_doctors, container, false);
+        tabLayout = view.findViewById(R.id.tab_layout);
+        viewPager2 = view.findViewById(R.id.view_pager);
+        myViewPagerAdapter = new PageAdapter(this);
+        viewPager2.setAdapter(myViewPagerAdapter);
+        TabLayout.Tab initialTab = tabLayout.getTabAt(0);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
+        return view;
+    }
+}
