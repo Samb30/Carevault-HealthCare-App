a6b7aad48b4190da4678ddbd74c897bfc1b7967b
UDAY B <udaybhati134@gmail.com>
2024-01-23 03:11:28 +0530

    frontend work of home page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,5 +65,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.squareup.picasso:picasso:2.8")
+    implementation ("androidx.cardview:cardview:1.0.0")
+
 
 }
